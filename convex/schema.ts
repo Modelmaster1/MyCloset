841d3abb0c82e08826b00f60ec1b513145420a64
Paul <developerpaul1@gmail.com>
2025-07-04 17:21:28 +0200

    schema done

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,12 +1,28 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-// The schema is entirely optional.
-// You can delete this file (schema.ts) and the
-// app will continue to work.
-// The schema provides more precise TypeScript types.
+
 export default defineSchema({
-  numbers: defineTable({
-    value: v.number(),
+  
+  clothingInfoItems: defineTable({
+    pic: v.string(), // url of the picture
+    colors: v.array(v.string()),
+    brand: v.string(),
+    type: v.array(v.string()),
+    user: v.string(),
+
+    items: v.array(v.id("clothingItems")),
+  }),
+
+  clothingItems: defineTable({
+    info: v.id("clothingInfoItems"),
+    location: v.array(v.object({
+      name: v.id("locations"),
+    })),
+    lost: v.optional(v.number()),
+  }),
+
+  locations: defineTable({
+    name: v.string(),
   }),
 });
